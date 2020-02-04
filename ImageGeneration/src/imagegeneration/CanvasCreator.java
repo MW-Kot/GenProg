cececca004c47ffd53937939a4a4c3ce525e2859
Max Whitmore <maxtwsm@gmail.com>
2020-02-04 07:15:53 -0500

    ImageGeneration project

---
+++ ImageGeneration/src/imagegeneration/CanvasCreator.java
@@ -0,0 +1,66 @@
+package imagegeneration;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author me
+ */
+public class CanvasCreator extends Application{
+    
+    private static Pixel[][]    pixels_;
+    private static int          width_;
+    private static int          height_;
+    private static final Canvas canvas_ = new Canvas();
+    //private static boolean      sharp_;
+    
+    public void CanvasCreator(Pixel[][] pixels)
+    {
+        pixels_ = pixels;
+        width_  = pixels_.length;
+        height_ = pixels_[0].length;
+        //sharp_  = sharp;
+        
+        genImage();
+    }
+    
+    public static void genImage() //pass width, height, Pixel array
+    {
+        canvas_.setHeight(height_);
+        canvas_.setWidth(width_);
+        
+        PixelWriter pw = canvas_.getGraphicsContext2D().getPixelWriter();
+        
+        for(int x = 0; x < width_; x++)
+        {
+            for(int y = 0; y < height_; y++)
+            {
+                //canvas.getGraphicsContext2D().getPixelWriter();
+                pw.setColor(x, y, Color.rgb(pixels_[x][y].gr(), pixels_[x][y].gg(), pixels_[x][y].gb(), pixels_[x][y].ga()));
+                
+                //https://stackoverflow.com/questions/27846659/how-to-draw-an-1-pixel-line-using-javafx-canvas
+                //https://stackoverflow.com/questions/28417623/the-fastest-filling-one-pixel-in-javafx
+            }
+        }
+        
+                
+
+    }//end genImage
+    
+    @Override
+    public void start(Stage primaryStage)
+    {
+        genImage();
+        VBox root = new VBox(5, canvas_);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+    
+    
+}
---
+++ ImageGeneration/src/imagegeneration/ImageGeneration.java
@@ -0,0 +1,101 @@
+
+package imagegeneration;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.Screen;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+
+//bg 34,38,44
+//bt 121,152,255
+//bt? 0,76,153
+//fg 210,210,210
+//tf 54,58,64
+
+
+
+
+public class ImageGeneration extends Application {
+    
+    
+    static Color bg = Color.rgb(34,  38,  44);
+    static Color fg = Color.rgb(210, 210, 210);
+    static Color bt = Color.rgb(121, 152, 255);
+    static Color tf = Color.rgb(54,  58,  64);
+
+    @Override
+    public void start(Stage primaryStage) 
+    {
+        
+        
+        primaryStage.setTitle("Mapper");
+        
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        grid.setStyle("-fx-background-color: 34,38,44");
+
+
+        
+
+        
+        //Group root = new Group();
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+        Scene scene = new Scene(grid, bounds.getWidth()/2, bounds.getHeight()/2, bg);
+
+        Text info = new Text("Select a button below to generate an image map.");
+        info.setX(bounds.getWidth()/5);
+        info.setY(bounds.getHeight()/5);
+        info.setFont(Font.font("Ubuntu", FontWeight.BOLD, FontPosture.REGULAR, 25));
+        info.setFill(fg);
+        grid.add(info, 0,0);
+        
+        
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Attempting to launch new windo...");
+                ConfigHandler config = new ConfigHandler();
+                
+                config.start(primaryStage);
+            }
+        });
+
+        grid.add(btn, 0, 2);
+        
+        
+
+        
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
